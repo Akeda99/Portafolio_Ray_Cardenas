aaaeb845ff96d32dd0aaf95128b33b3f2010be79
Akeda99 <raykevin1999@gmail.com>
2022-12-06 17:02:38 -0500

    another commmit

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import { headerScroll } from './headerScroll.js';
 
+const iconMenu= document.querySelector(".icon_menu");
+const menu= document.querySelector(".menu2");
+
+iconMenu.addEventListener("click", function(){
+    console.log(menu.classList.toggle("menu_show"));
+});
+menu.addEventListener("click",function(){
+    console.log(menu.classList.toggle("menu_show"));
+});
 document.addEventListener('DOMContentLoaded', function () {
     headerScroll()
 })
